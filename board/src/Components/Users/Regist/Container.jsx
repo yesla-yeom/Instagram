a72064b3c8e57ff4647d8d3c214cd531cc440d79
예나나 <111760323+yesla-yeom@users.noreply.github.com>
2023-03-13 16:30:07 +0900

    23.3.13. update

--- board/src/Components/Users/Regist/Container.jsx
+++ board/src/Components/Users/Regist/Container.jsx
@@ -3,11 +3,13 @@ import axios from "axios";
 
 const RegistContainer = ({ theme }) => {
   const onClick = (userId, userPw, userName) => {
-    axios.post("http://192.168.0.107:8080/api/users/regist", {
+    const data = axios.post("http://192.168.0.107:8080/api/users/regist", {
       userId,
       userPw,
       userName,
     });
+
+    window.location.reload();
   };
 
   return <RegistComponent onClick={onClick} theme={theme} />;
